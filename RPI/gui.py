8a5e540b1e1bb3124ec441267182f822a0b8b588
Chathura Jayasanka <cjayasanka93@gmail.com>
2018-06-30 20:47:10 +0530

    background + menu code for gui

--- RPI/gui.py
+++ RPI/gui.py
@@ -1,8 +1,26 @@
-import Tkinter as tk
+from Tkinter import *
+from tkFileDialog   import askopenfilename
 
-root = tk.Tk()
+def NewFile():
+    print "New File!"
+def OpenFile():
+    name = askopenfilename()
+    print name
+def About():
+    print "This is a simple example of a menu"
+    
+root = Tk()
+menu = Menu(root)
+root.config(menu=menu)
+filemenu = Menu(menu)
+menu.add_cascade(label="File", menu=filemenu)
+filemenu.add_command(label="New", command=NewFile)
+filemenu.add_command(label="Open...", command=OpenFile)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
 
-w = tk.Label(root, text="Hello Tkinter!")
-w.pack()
+helpmenu = Menu(menu)
+menu.add_cascade(label="Help", menu=helpmenu)
+helpmenu.add_command(label="About...", command=About)
 
-root.mainloop()
+mainloop()
